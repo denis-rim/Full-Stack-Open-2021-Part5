0553f2afacbf759406d910dde58f4c7d1bf581a7
denis-rim <denis.aurorim@gmail.com>
2021-05-24 13:22:04 +0300

    5.7* Blog list frontend, step7

--- client/src/components/Blog.js
+++ client/src/components/Blog.js
@@ -1,8 +1,43 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from "react";
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
 
-export default Blog
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const displayShortInfo = () => (
+    <div>
+      {blog.title} {blog.author}
+    </div>
+  );
+
+  const displayFullInfo = () => (
+    <div>
+      <p>Title: {blog.title}</p>
+      <p>{blog.url}</p>
+      <p>
+        Likes: {blog.likes} <button>like</button>
+      </p>
+      <p>{blog.author}</p>
+    </div>
+  );
+
+  console.log(blog);
+
+  return (
+    <div style={blogStyle}>
+      {!visible ? displayShortInfo() : displayFullInfo()}
+      <button onClick={toggleVisibility}>{!visible ? "View" : "Hide"}</button>
+    </div>
+  );
+};
+export default Blog;
